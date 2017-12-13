62dc25fcebcb48175ea30c059ecc848612b6bd66
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2017-12-13 16:53:16 +0000

    Boids Rotate Slowly
    -Updateded Boid class with rotation limitations.
    -Changed ShootingPower.

--- Mechanics/Boid.cs
+++ Mechanics/Boid.cs
@@ -10,6 +10,7 @@ namespace TankProject
     class Boid : GameObject
     {
         private const float MAX_VELOCITY = 0.02f;
+        private const float MAX_ROTATION = 1.0f;
         private Vector3 seekVelocity;
         private Vector3 acceleration;
 
@@ -29,6 +30,24 @@ namespace TankProject
                     velocity = relativeForward * 0.01f;
                 seekVelocity = Vector3.Normalize(targetPosition - position) * MAX_VELOCITY;
                 acceleration = seekVelocity - velocity;
+
+                double cosSteeringAngle;
+                if (Vector3.Dot(Right, acceleration) < 0)
+                    cosSteeringAngle = Vector3.Dot(Vector3.Normalize(velocity), Vector3.Normalize(acceleration));
+                else
+                    cosSteeringAngle = Vector3.Dot(Vector3.Normalize(acceleration), Vector3.Normalize(velocity));
+                double angle = Math.Acos(cosSteeringAngle);
+
+                if(angle > MAX_ROTATION)
+                {
+                    Matrix angleRotation = Matrix.CreateFromAxisAngle(Up, MAX_ROTATION - (float)angle);
+                    acceleration = Vector3.Transform(acceleration, angleRotation);
+                }
+                else if (angle < -MAX_ROTATION)
+                {
+                    Matrix angleRotation = Matrix.CreateFromAxisAngle(Up, -MAX_ROTATION - (float)angle);
+                    acceleration = Vector3.Transform(acceleration, angleRotation);
+                }
 
                 velocity += acceleration * (float)deltaTime.ElapsedGameTime.TotalSeconds;
                 if (velocity.Length() > MAX_VELOCITY)
