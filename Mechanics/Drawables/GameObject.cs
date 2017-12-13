0ccbc1fad8dc30990977e77f9a04cc2917bb8d90
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2017-12-13 17:23:36 +0000

    Fixed some leftovers. Made rotations work.
    -Cleared signes in Boid.
    -Cleaned cod.

--- Mechanics/Drawables/GameObject.cs
+++ Mechanics/Drawables/GameObject.cs
@@ -10,7 +10,6 @@ namespace TankProject
         internal Vector3 position;
         internal Vector3 rotation;
         internal Vector3 velocity;
-        internal float mass;
 
         internal Vector3 relativeForward, relativeRight;
         internal Vector3 Up, Forward, Right;
@@ -20,12 +19,11 @@ namespace TankProject
 
         //--------------------Constructors--------------------//
 
-        internal GameObject(Vector3 position, Vector3 rotation, Vector3 velocity, float mass)
+        internal GameObject(Vector3 position, Vector3 rotation, Vector3 velocity)
         {
             this.position = position;
             this.velocity = velocity;
             this.rotation = rotation;
-            this.mass = mass;
             rotationMatrix = Matrix.CreateFromYawPitchRoll(rotation.X, rotation.Y, rotation.Z);
             translationMatrix = Matrix.CreateTranslation(position);
         }
